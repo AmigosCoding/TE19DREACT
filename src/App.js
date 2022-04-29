672584325c2e4a5ac0adcb9e9ad251ee144a8c47
Abdirahman Dayib <Abdirahman.Dayib@ga.ntig.se>
2022-04-29 14:08:46 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+ 
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(response => response.json())
+   .then(data => setUsers(data));
+   console.log(users);
+  
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <table>
+  <tr>
+    <th>Company</th>
+    <th>Phone</th>
+    <th>City</th>
+  </tr>
+  {
+    users.map(user => {
+      return(
+        <tr>
+        <td>{user.company.name}</td>
+        <td>{user.phone}</td>
+        <td>{user.address.city}</td>
+      </tr>
+      )
+    })
+
+
+  }
+
+  
+</table>
+     
+      
     </div>
   );
 }
